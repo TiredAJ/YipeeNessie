f11323b85540e334abec171842671d27cf945ffe
AJ <aletha.e.jones@gmail.com>
2024-01-11 22:57:59 +0000

    something idk

--- YippeeNessie/ViewModels/MainViewModel.cs
+++ YippeeNessie/ViewModels/MainViewModel.cs
@@ -1,9 +1,69 @@
-﻿namespace YippeeNessie.ViewModels
+﻿// Ignore Spelling: Nessie
+
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using LibVLCSharp.Shared;
+using ReactiveUI;
+using System;
+using System.Diagnostics;
+
+namespace YippeeNessie.ViewModels;
+
+public class MainViewModel : ViewModelBase
 {
-    public class MainViewModel : ViewModelBase
+    private static LibVLC LVLC;
+
+    private Media YippeeMedia;
+
+    public Uri YippeeAudio { get; }
+    private Uri NessieLoc =>
+        new Uri("avares://YippeeNessie/Assets/Green_Nessie.png");
+
+    private Bitmap _Nessie;
+    public Bitmap Nessie
     {
-#pragma warning disable CA1822 // Mark members as static
-        public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+        get => _Nessie;
+
+        private set
+        { this.RaiseAndSetIfChanged(ref _Nessie, value); }
+    }
+
+    private MediaPlayer MP { get; }
+
+    private bool _ShowNessie = false;
+    public bool ShowNessie
+    {
+        get => _ShowNessie;
+        set
+        { this.RaiseAndSetIfChanged(ref _ShowNessie, value); }
+    }
+
+    public MainViewModel()
+    {
+        YippeeAudio = new Uri("avares://YippeeNessie/Assets/Yippee.wav");
+        Nessie = new Bitmap(AssetLoader.Open(NessieLoc));
+
+        LVLC = new LibVLC();
+
+        YippeeMedia = new Media(LVLC, YippeeAudio.AbsolutePath);
+
+        LVLC.Log += LVLC_Log;
+    }
+
+    private void LVLC_Log(object? sender, LogEventArgs e)
+    { Debug.WriteLine(e.FormattedLog); }
+
+    public void NessieTime()
+    {
+        using (var MP = new MediaPlayer(YippeeMedia))
+        {
+            MP.Play();
+
+            if (!MP.WillPlay)
+            {
+                throw new Exception("Won't play audio");
+            }
+            MP.Volume += 1;
+        }
     }
 }
